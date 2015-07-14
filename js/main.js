af37c03306c69015a7d055b97ab5cecee3441b27
semerdj <jsemer@gmail.com>
2015-07-13 21:20:39 -0700

    move foo.topojson to data folder

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@
   var g = svg.append("g");
 
   queue()
-      .defer(d3.json, "../d3-zip-code-choropleth/shapefile/foo.topojson")
+      .defer(d3.json, "../d3-zip-code-choropleth/data/foo.topojson")
       // .defer(d3.json, "../data/zip_code_crs84.topojson")
       .defer(d3.csv, "../d3-zip-code-choropleth/data/ca_population_2010.csv", function(d) { rateById.set(d.zip.toString(), +d.population); })
       .await(ready);
